8e139ea5e76c1313a8a2cb5df0506c4eea76805d
kjh4357 <kjh4357@gmail.com>
2023-03-14 14:05:51 +0900

    리소스 api, local 분기 완료

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -3,21 +3,63 @@ import { initReactI18next } from "react-i18next";
 import en from "./en/common.json";
 import ko from "./ko/common.json";
 
-// const resources = {
-//   en: { translation: en },
-//   ko: { translation: ko },
-// };
+import backend from "i18next-http-backend";
+import Axios from "axios";
+
 const resources = {
   en: { translation: en },
   ko: { translation: ko },
 };
-i18next.use(initReactI18next).init({
-  resources,
-  lng: "ko",
-  interpolation: {
-    escapeValue: false,
+const loadResources = async (lng: any) => {
+  return await Axios.get(
+    "https://api.stage.staika.io/services/board/api/locale-messages?clientId=GAZAGO&sheetName=WEB_GAZAGO"
+  )
+    .then((res) => {
+      return res.data;
+    })
+    .catch((error) => {
+      const errRes = error.toJSON();
+
+      return errRes;
+    });
+};
+
+const backendOptions = {
+  loadPath: "{{lng}}|{{ns}}",
+  request: (options: any, url: any, payload: any, callback: any) => {
+    const [lng] = url.split("|");
+    loadResources(lng).then((response) => {
+      if (response.status) {
+        callback(null, {
+          // @ts-ignore
+          data: resources[lng].translation,
+          status: 200,
+        });
+      } else {
+        callback(null, {
+          data: response[lng],
+          status: 200,
+        });
+      }
+    });
   },
-});
+};
+
+i18next
+  .use(backend)
+  .use(initReactI18next)
+  .init({
+    backend: backendOptions,
+    fallbackLng: "ko",
+    // debug: true,
+    ns: ["common"],
+    defaultNS: "common",
+    keySeparator: false,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
 i18next.on("languageChanged", (lng) => {
   document.documentElement.setAttribute("lang", lng);
 });
