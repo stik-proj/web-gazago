14147509cc0170a4f6bbc1a5c6a899fc9aa49788
kjh4357 <kjh4357@gmail.com>
2023-03-14 16:51:46 +0900

    node env 환경 변수 설정

--- translation.js
+++ translation.js
@@ -3,11 +3,13 @@ var fs = require("fs");
 const { default: axios } = require("axios");
 
 const getTranslationJson = async () => {
+  const apiEnv =
+    process.env.NODE_ENV === "production"
+      ? "https://api.staika.io"
+      : "https://api.stage.staika.io";
   await axios
     .get(
-      `${
-        process.env.REACT_APP_BASE_URL
-      }/services/board/locale-messages?clientId=GAZAGO&sheetName=WEB_GAZAGO`
+      `${apiEnv}/services/board/api/locale-messages?clientId=GAZAGO&sheetName=WEB_GAZAGO`
     )
     .then((res) => {
       const originData = res.data;
