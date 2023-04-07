9c7853fed813fb8f00cd8821fcd5c0b3a6b7c418
EZ-woong <tjsdnd3955@eztechfin.co.kr>
2023-04-07 11:29:27 +0900

    - footer 영문 버전 추가

--- src/presentations/views/Footer.tsx
+++ src/presentations/views/Footer.tsx
@@ -23,35 +23,31 @@ export default function Footer() {
         <p className="footer-text md:text-[1.4rem] lg:text-[1.6rem]">
           {t(`footer.desc3`)}
         </p>
-        {i18n.language === "ko" ? (
-          <div className="mt-[2.8rem] md:mt-[3.5rem] lg:mt-[2.8rem]">
-            <a
-              className="inline-block"
-              href={data.terms}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <p className="footer-text-terms md:text-[1.4rem] md:leading-[1.4rem] lg:text-[1.6rem] lg:leading-[1.6rem]">
-                {t(`footer.terms`)}
-              </p>
-            </a>
-            <span className="text-[#6B6B6B] text-[1.3rem] font-[600] ml-[0.8rem] lg:ml-[1.2rem] md:text-[1.4rem] md:leading-[1.4rem] lg:text-[1.6rem] lg:leading-[1.6rem]">
-              |
-            </span>
-            <a
-              className="inline-block"
-              href={data.privacy}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <p className="footer-text-terms ml-[0.8rem] lg:ml-[1.2rem] md:text-[1.4rem] md:leading-[1.4rem] lg:text-[1.6rem] lg:leading-[1.6rem]">
-                {t(`footer.privacy`)}
-              </p>
-            </a>
-          </div>
-        ) : (
-          ""
-        )}
+        <div className="mt-[2.8rem] md:mt-[3.5rem] lg:mt-[2.8rem]">
+          <a
+            className="inline-block"
+            href={data.terms}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <p className="footer-text-terms md:text-[1.4rem] md:leading-[1.4rem] lg:text-[1.6rem] lg:leading-[1.6rem]">
+              {t(`footer.terms`)}
+            </p>
+          </a>
+          <span className="text-[#6B6B6B] text-[1.3rem] font-[600] ml-[0.8rem] lg:ml-[1.2rem] md:text-[1.4rem] md:leading-[1.4rem] lg:text-[1.6rem] lg:leading-[1.6rem]">
+            |
+          </span>
+          <a
+            className="inline-block"
+            href={data.privacy}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <p className="footer-text-terms ml-[0.8rem] lg:ml-[1.2rem] md:text-[1.4rem] md:leading-[1.4rem] lg:text-[1.6rem] lg:leading-[1.6rem]">
+              {t(`footer.privacy`)}
+            </p>
+          </a>
+        </div>
       </div>
     </div>
   );
