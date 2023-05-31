9dee3db7a825530c88202cefd09e4ca4a505b227
kjh4357 <kjh4357@gmail.com>
2023-05-31 17:54:08 +0900

    사용하지 않는 코드 제거

--- src/presentations/views/Footer.tsx
+++ src/presentations/views/Footer.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from "react-i18next";
 
 export default function Footer() {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   const data = {
     terms: t("footer.terms.link"),
